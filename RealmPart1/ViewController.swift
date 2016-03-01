e584b793b1dba56cf5e3ad88b2dfcb639941c9f3
kevadiya.vishal@gmail.com <kevadiya.vishal@gmail.com>
2016-03-01 18:43:26 +0530

    Set The ViewController BGColor

--- RealmPart1/ViewController.swift
+++ RealmPart1/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.purpleColor()
 
     }
 
